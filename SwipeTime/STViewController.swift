cd273ddf46b7dcb2badb084325339cea1c33e7e8
Alan Kantz <akantz@thoracic.org>
2016-06-28 19:56:55 -0400

    Implemented a countdown timer activated with any direction of swipe and cancelled with the cancel button.

--- SwipeTime/STViewController.swift
+++ SwipeTime/STViewController.swift
@@ -9,17 +9,14 @@
 import UIKit
 
 class STViewController: UIViewController {
-
     
-    var startTime = 30
+    
     var timer = NSTimer()
+    var counter = 3000
     
-    func millisecondsToMinutesSecondsMilliseconds(time: Int) -> ([Int]) {
-        return [time / 6000, (time / 100) % 60, time % 100]
-    }
-    
-    func formatTime(time: [Int]) -> (String) {
+    func formatTime(time: Int) -> (String) {
         
+        let timeBlocks = [time / 6000, (time / 100) % 60, time % 100]
         let numberFormatter = NSNumberFormatter()
         numberFormatter.formatWidth = 2
         numberFormatter.paddingCharacter = "0"
@@ -27,7 +24,7 @@ class STViewController: UIViewController {
         var timeAsString = ["", "", ""]
         
         var i = 0
-        for block in time {
+        for block in timeBlocks {
             timeAsString[i] = numberFormatter.stringFromNumber(block)!
             i += 1
         }
@@ -35,36 +32,71 @@ class STViewController: UIViewController {
         return timeAsString[0] + ":" + timeAsString[1] + "." + timeAsString[2]
         
     }
-
+    
+    func startTimer() {
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target:self, selector: #selector(STViewController.incrementCounter), userInfo: nil, repeats: true)
+    }
+    
+    func incrementCounter() {
+        counter -= 1
+        timeDisplay.text = formatTime(counter)
+        if counter <= 0 {
+            stopTimer()
+        }
+    }
+    
+    func stopTimer() {
+        timer.invalidate()
+        counter = 3000
+        timeDisplay.text = formatTime(counter)
+    }
+    
     
     @IBOutlet var timeDisplay: UILabel!
-    @IBOutlet var cancelTimer: UIButton!
-    @IBOutlet var rightStartTimer: UISwipeGestureRecognizer!
-    @IBOutlet var leftStartTimer: UISwipeGestureRecognizer!
-    @IBOutlet var upStartTimer: UISwipeGestureRecognizer!
-    @IBOutlet var downStartTimer: UISwipeGestureRecognizer!
+    
+    @IBAction func cancelTimer(sender: AnyObject) {
+        stopTimer()
+    }
+    
+    @IBAction func swipeRight(sender: AnyObject) {
+        startTimer()
+    }
 
+    @IBAction func swipeLeft(sender: AnyObject) {
+        startTimer()
+    }
+
+    @IBAction func swipeUp(sender: AnyObject) {
+        startTimer()
+    }
+    
+    @IBAction func swipeDown(sender: AnyObject) {
+        startTimer()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        
+        stopTimer()
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
