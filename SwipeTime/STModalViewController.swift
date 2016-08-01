024463eb44046b0daff53319885e994ef9ac2ae8
Alan Kantz <akantz@thoracic.org>
2016-07-31 20:06:48 -0400

    Fixed most of the small things on my roadmap, then added registration of the swipetime:// URL scheme.

--- SwipeTime/STModalViewController.swift
+++ SwipeTime/STModalViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-class STModalViewController: UIViewController {
-
+class STModalViewController: UIViewController, UITextFieldDelegate {
+    
     @IBOutlet var timeField: UITextField!
     @IBOutlet var doneButton: UIBarButtonItem!
     @IBAction func cancel(sender: UIBarButtonItem) {
+        
+        // Make sure to get rid of the keyboard before dismissing the view controller
+        timeField.resignFirstResponder()
         dismissViewControllerAnimated(true, completion: nil)
     }
     
@@ -20,22 +23,36 @@ class STModalViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        timeField.delegate = self
         timeField.becomeFirstResponder()
         
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        
+        // This is taken from the Internet. The first bit prevents a crash-on-undo that happens when iOS tries to undo to a change that was blocked by shouldChangeCharactersInRange = false. The second bit only allows characters to be added to the text field if the current number of characters plus the number to be added (minus the range of characters being replaced) is <= 0.
+        
+        let currentCharacterCount = textField.text?.characters.count ?? 0
+        if (range.length + range.location > currentCharacterCount) {
+            return false
+        }
+        let newLength = currentCharacterCount + string.characters.count - range.length
+        return newLength <= 3
+    }
+    
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        timeField.resignFirstResponder()
+        
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
         
