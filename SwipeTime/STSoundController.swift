ecfb5a3b6bcea130ac0545c9563c9a4c04adb81e
Alan Kantz <charlottekantz@alans-air.fios-router.home>
2016-12-10 12:39:18 -0500

    Works on iPad; is purple

--- SwipeTime/STSoundController.swift
+++ SwipeTime/STSoundController.swift
@@ -12,21 +12,24 @@ import AudioToolbox
 class STSoundController: NSObject {
 
     
-    // Chime when timer starts
     let firstSoundURLRef = CFBundleCopyResourceURL(CFBundleGetMainBundle(), "AudioCue_01" as CFString!, "aif" as CFString!, nil)
     var firstSoundID: SystemSoundID = 0
- 
-    func playFirstSound() {
-        AudioServicesCreateSystemSoundID(firstSoundURLRef!, &firstSoundID)
-        AudioServicesPlayAlertSound(firstSoundID)
-    }
-    
-    // Chime when timer ends
     let secondSoundURLRef = CFBundleCopyResourceURL(CFBundleGetMainBundle(), "AudioCue_02" as CFString!, "aif" as CFString!, nil)
     var secondSoundID: SystemSoundID = 1
     
-    func playSecondSound() {
+ 
+    override init() {
+        // Prep both sounds
+        AudioServicesCreateSystemSoundID(firstSoundURLRef!, &firstSoundID)
         AudioServicesCreateSystemSoundID(secondSoundURLRef!, &secondSoundID)
+    }
+    
+    func playFirstSound() {
+        AudioServicesPlayAlertSound(firstSoundID)
+    }
+    
+    
+    func playSecondSound() {
         AudioServicesPlayAlertSound(secondSoundID)
     }
     
