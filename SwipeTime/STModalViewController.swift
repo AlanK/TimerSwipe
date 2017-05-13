1924f7f1aff9e67ca6af49e1d2b979c2b4fad9e2
Alan Kantz <charlottekantz@alans-air.fios-router.home>
2017-05-13 11:08:54 -0400

    Mostly whitespace fixes.

--- SwipeTime/STModalViewController.swift
+++ SwipeTime/STModalViewController.swift
@@ -10,12 +10,11 @@ import UIKit
 
 class STModalViewController: UIViewController, UITextFieldDelegate {
     var userSelectedTime: Int?
-    
 
     @IBOutlet var timeField: UITextField!
     @IBOutlet var doneButton: UIBarButtonItem!
+    
     @IBAction func cancel(_ sender: UIBarButtonItem) {
-        
         // Make sure to get rid of the keyboard before dismissing the view controller
         timeField.resignFirstResponder()
         dismiss(animated: true, completion: nil)
@@ -23,19 +22,15 @@ class STModalViewController: UIViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         timeField.delegate = self
         timeField.becomeFirstResponder()
     }
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        
         // This is taken from the Internet. The first bit prevents a crash-on-undo that happens when iOS tries to undo to a change that was blocked by shouldChangeCharactersInRange = false. The second bit only allows characters to be added to the text field if the current number of characters plus the number to be added (minus the range of characters being replaced) is <= 0.
-        
         let currentCharacterCount = textField.text?.characters.count ?? 0
-        if (range.length + range.location > currentCharacterCount) {
-            return false
-        }
+        
+        guard (range.length + range.location <= currentCharacterCount) else {return false}
         let newLength = currentCharacterCount + string.characters.count - range.length
         return newLength <= 3
     }
@@ -47,5 +42,5 @@ class STModalViewController: UIViewController, UITextFieldDelegate {
         
         guard let userTime = timeField.text, let userTimeInCentiseconds = Int(userTime + "00"), userTimeInCentiseconds > 0 else {return}
         userSelectedTime = userTimeInCentiseconds
-    }    
+    }
 }
