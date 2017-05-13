7607ce50575a1f0730d17a796699f6c773581dc5
Alan Kantz <charlottekantz@alans-air.fios-router.home>
2017-05-13 11:24:21 -0400

    Rearranged STSoundController now that I understand how in-out parameters work.

--- SwipeTime/STSoundController.swift
+++ SwipeTime/STSoundController.swift
@@ -9,27 +9,22 @@
 import AudioToolbox
 
 class STSoundController {
-
+    var firstSound: SystemSoundID = 0
+    var secondSound: SystemSoundID = 1
     
-    let firstSoundURLRef = CFBundleCopyResourceURL(CFBundleGetMainBundle(), "AudioCue_01" as CFString!, "aif" as CFString!, nil)
-    var firstSoundID: SystemSoundID = 0
-    let secondSoundURLRef = CFBundleCopyResourceURL(CFBundleGetMainBundle(), "AudioCue_02" as CFString!, "aif" as CFString!, nil)
-    var secondSoundID: SystemSoundID = 1
-    
- 
     init() {
-        // Prep both sounds
-        AudioServicesCreateSystemSoundID(firstSoundURLRef!, &firstSoundID)
-        AudioServicesCreateSystemSoundID(secondSoundURLRef!, &secondSoundID)
+        let firstSoundURLRef = CFBundleCopyResourceURL(CFBundleGetMainBundle(), "AudioCue_01" as CFString!, "aif" as CFString!, nil)
+        AudioServicesCreateSystemSoundID(firstSoundURLRef!, &firstSound)
+        
+        let secondSoundURLRef = CFBundleCopyResourceURL(CFBundleGetMainBundle(), "AudioCue_02" as CFString!, "aif" as CFString!, nil)
+        AudioServicesCreateSystemSoundID(secondSoundURLRef!, &secondSound)
     }
     
     func playFirstSound() {
-        AudioServicesPlayAlertSound(firstSoundID)
+        AudioServicesPlayAlertSound(firstSound)
     }
-    
     
     func playSecondSound() {
-        AudioServicesPlayAlertSound(secondSoundID)
+        AudioServicesPlayAlertSound(secondSound)
     }
-    
 }
