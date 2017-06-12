01fb844ecb0b9b84e91b8d9756855204be63301a
Alan Kantz <alan.kantz@gmail.com>
2017-06-12 16:57:01 -0400

    Main nav stack now has a custom navigation controller.

---
+++ SwipeTime/MainNavController.swift
@@ -0,0 +1,20 @@
+//
+//  MainNavController.swift
+//  SwipeTime
+//
+//  Created by Alan Kantz on 6/12/17.
+//  Copyright © 2017 Alan Kantz. All rights reserved.
+//
+
+import UIKit
+
+class MainNavController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
