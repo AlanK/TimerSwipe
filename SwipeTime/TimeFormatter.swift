c278f4ebaed55014af6744453f6be37c9869cfa3
Alan Kantz <alan.kantz@gmail.com>
2017-07-01 23:46:28 -0400

    Switched from a really ugly NumberFormatter to a trivial DateFormatter.

--- SwipeTime/TimeFormatter.swift
+++ SwipeTime/TimeFormatter.swift
@@ -10,25 +10,17 @@ import UIKit
 
 /// Formats a centisecond integer into a "00:00.00" string ready to display
 struct TimeFormatter {
-    // Can't use DateComponentsFormatter because we need formatting of hundredths of seconds
-    private let numberFormatter = NumberFormatter()
+    // DateFormatter supports hundredths of a second, so we don't have to use NumberFormatter
+    private let dateFormatter = DateFormatter()
     
-    // Format each block of numbers as two digits padded with 0s if necessary
     init() {
-        numberFormatter.formatWidth = 2
-        numberFormatter.paddingCharacter = "0"
+        dateFormatter.dateFormat = "mm:ss.SS"
     }
     
     /// Takes a centisecond integer and returns a string in "00:00.00" format
     func formatTime(_ centiseconds: Int) -> String {
-        // Break centisecond integer into [minutes, seconds, hundredths-of-a-second]
-        let integerComponents = [centiseconds / K.centisecondsPerMinute, (centiseconds / K.centisecondsPerSecond) % K.secondsPerMinute, centiseconds % K.centisecondsPerSecond]
-        // Build string array of [minutes, seconds, hundredths]
-        var timeAsString = ["", "", ""]
-        for index in 0...2 {
-            timeAsString[index] = numberFormatter.string(from: NSNumber(value: integerComponents[index])) ?? "00"
-        }
-        // Return a string of minutes:seconds.hundredths
-        return timeAsString[0] + ":" + timeAsString[1] + "." + timeAsString[2]
+        let timeInterval = Double(centiseconds)/K.centisecondsPerSecondDouble
+        let usableDate = Date(timeIntervalSinceReferenceDate: timeInterval)
+        return dateFormatter.string(from: usableDate)
     }
 }
