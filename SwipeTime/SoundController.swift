adb168d0f96be4d22c6c280d3c333cdd48c3c83e
Alan Kantz <alan.kantz@gmail.com>
2017-10-21 12:00:05 -0400

    Made audioPlayer dictionary values non-optional; simpler player initialization; made vibrate() a nested function

--- SwipeTime/SoundController.swift
+++ SwipeTime/SoundController.swift
@@ -18,27 +18,23 @@ enum AudioCue: String {
 /// Handles sounds for the main view of the app
 struct SoundController {
     private let audioSession = AVAudioSession()
-    // Initializing an AVAudioPlayer is failable, so these need to be optionals
-    private var sounds = [AudioCue : AVAudioPlayer?]()
+    private var audioPlayers = [AudioCue : AVAudioPlayer]()
     
     init() {
         /**
          Create an `AVAudioPlayer` from a filename
-         - parameter file: A filename for a file in the main bundle
-         - returns: An `AVAudioPlayer` configured to play the named file
+         - parameter cue: An audio cue
+         - returns: An `AVAudioPlayer` configured to play the audio cue
          */
-        func initializePlayer(with file: String) -> AVAudioPlayer? {
-            guard let path = Bundle.main.path(forResource: file, ofType: nil), let player = try? AVAudioPlayer(contentsOf: URL(fileURLWithPath: path)) else {
-                print("Could not initialize \(file).")
-                return nil
-            }
-            return player
+        func newPlayer(_ cue: AudioCue) -> AVAudioPlayer? {
+            guard let path = Bundle.main.path(forResource: cue.rawValue, ofType: nil) else {return nil}
+            return try? AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
         }
         
         // Initialize the audio players. Kinda silly how verbose this is
-        sounds[.start] = initializePlayer(with: AudioCue.start.rawValue)
-        sounds[.end] = initializePlayer(with: AudioCue.end.rawValue)
-        sounds[.die] = initializePlayer(with: AudioCue.die.rawValue)
+        audioPlayers[.start] = newPlayer(.start)
+        audioPlayers[.end] = newPlayer(.end)
+        audioPlayers[.die] = newPlayer(.die)
         // Configure audio session to mix with background music
         do {
             try audioSession.setCategory(AVAudioSessionCategoryAmbient, mode: AVAudioSessionModeDefault, options: [])
@@ -56,27 +52,28 @@ struct SoundController {
         
         // When activating the audio session, it is nice to prepare the sounds proactively.
         guard active else {return}
-        for sound in sounds {
-            sound.value?.prepareToPlay()
+        for audioPlayer in audioPlayers {
+            audioPlayer.value.prepareToPlay()
         }
     }
     
-    /// Vibrate without playing a sound
-    private func vibrate() {
-        AudioServicesPlaySystemSound(kSystemSoundID_Vibrate)
-    }
-    
-    /// Play the appropriate audio and vibration cue
+    /**
+     Play the appropriate audio and vibration cue
+     - parameter cue: Which audio cue should play
+     */
     func play(_ cue: AudioCue) {
-        guard let sound = sounds[cue], let audio = sound else {return}
-        audio.play()
+        /// Vibrate without playing a sound
+        func vibrate() {AudioServicesPlaySystemSound(kSystemSoundID_Vibrate)}
+        
+        guard let audioPlayer = audioPlayers[cue] else {return}
+        audioPlayer.play()
         vibrate()
-        audio.prepareToPlay()
+        audioPlayer.prepareToPlay()
         
         // Add an extra vibration for the warning cue
         guard cue == .die else {return}
         Timer.scheduledTimer(withTimeInterval: 1.0, repeats: false) {timer in
-            self.vibrate()
+            vibrate()
         }
     }
 }
