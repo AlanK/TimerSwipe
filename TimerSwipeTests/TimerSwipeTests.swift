06d28090d31721fc675dac724903181db4e929b5
Alan Kantz <alan.kantz@gmail.com>
2017-10-24 19:03:14 -0400

    Added a unit test build

---
+++ TimerSwipeTests/TimerSwipeTests.swift
@@ -0,0 +1,14 @@
+//
+//  TimerSwipeTests.swift
+//  TimerSwipeTests
+//
+//  Created by Alan Kantz on 10/24/17.
+//  Copyright © 2017 Alan Kantz. All rights reserved.
+//
+
+import XCTest
+@testable import TimerSwipe
+
+class TimerSwipeTests: XCTestCase {
+    
+}
