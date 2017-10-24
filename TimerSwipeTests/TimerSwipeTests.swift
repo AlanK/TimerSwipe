1af9babe086dd54597c6aa1196726ca468e00028
Alan Kantz <alan.kantz@gmail.com>
2017-10-24 19:04:47 -0400

    First unit test

--- TimerSwipeTests/TimerSwipeTests.swift
+++ TimerSwipeTests/TimerSwipeTests.swift
@@ -10,5 +10,10 @@ import XCTest
 @testable import TimerSwipe
 
 class TimerSwipeTests: XCTestCase {
-    
+    func inputViewInitializesInvisible() -> InputView {
+        let frame = CGRect(x: 0, y: 0, width: 500, height: 100)
+        let inputView = InputView.init(frame: frame, inputViewStyle: .default)
+        XCTAssertFalse(inputView.isVisible)
+        return inputView
+    }
 }
