fc62b8adb3d067fbe5dc7b6609dc712956d4d1ec
Alan Kantz <alan.kantz@gmail.com>
2017-11-05 16:09:56 -0500

    BROKEN: REMOVE ALL THE THINGS

--- SwipeTime/AppDelegate.swift
+++ SwipeTime/AppDelegate.swift
@@ -9,37 +9,6 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIApplicationDelegate {
     var window: UIWindow?
-    private var lastEnteredBackground: Date?
-    private let timeout: TimeInterval = 300.0
-
-    private var nav: NavController? {
-        return window?.rootViewController as? NavController
-    }
-    
-    private var stopwatchIntermediary: StopwatchIntermediary? {
-        return window?.rootViewController as? StopwatchIntermediary
-    }
-    
-    func applicationDidFinishLaunching(_ application: UIApplication) {
-        self.window?.tintColor = K.tintColor
-        // Shake-to-undo is too fiddly for a three-digit numbers-only text field, so lets turn it off
-        UIApplication.shared.applicationSupportsShakeToEdit = false
-    }
-    
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        lastEnteredBackground = Date()
-    }
-    
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        if let date = lastEnteredBackground, timeout < Date().timeIntervalSince(date) {
-            nav?.refreshViews()
-        }
-        lastEnteredBackground = nil
-    }
-    
-    func applicationWillTerminate(_ application: UIApplication) {
-        stopwatchIntermediary?.killTimer()
-    }
 }
