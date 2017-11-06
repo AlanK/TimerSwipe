07c8a1a29597e703e44e9141b5143f6aa293c1f7
Alan Kantz <alan.kantz@gmail.com>
2017-11-06 09:43:15 -0500

    Removing more garbage

--- SwipeTime/AppDelegate.swift
+++ SwipeTime/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  SwipeTime
-//
-//  Created by Alan Kantz on 6/24/16.
-//  Copyright © 2016 Alan Kantz. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
---
+++ SwipeTime/STSavedTimer.swift
@@ -0,0 +1,50 @@
+//
+//  STSavedTimer.swift
+//  SwipeTime
+//
+//  Created by Alan Kantz on 8/8/16.
+//  Copyright © 2016 Alan Kantz. All rights reserved.
+//
+
+import Foundation
+
+/// Model representation of a user-created timer
+class STSavedTimer: NSObject, NSCoding {
+    private static let centisecondsPerSecond = 100.0
+    /// Timer duration in seconds
+    let seconds: TimeInterval
+    /// Whether or not the timer is the user’s favorite
+    var isFavorite: Bool
+    
+    // Memberwise initializer enables NSCoding required convenience initializer
+    /// Create a timer of the specified seconds and favorite status
+    init(seconds: TimeInterval, isFavorite: Bool) {
+        self.seconds = seconds
+        self.isFavorite = isFavorite
+    }
+    
+    /// Create a timer of the specified centiseconds
+    init(seconds: TimeInterval) {
+        self.seconds = seconds
+        self.isFavorite = false
+    }
+    
+    /// Create a timer of the default duration
+    override convenience init() {
+        self.init(seconds: K.defaultDuration)
+    }
+    
+    // MARK: NSCoding
+    // This class serializes centiseconds as Int instead of seconds as TimeInterval. This is the result of bad model design when the app was first created and released. For now, centisecond-related legacy code has been quarantined in this part of the app. In the future, the model should be rewritten to serialize seconds as TimeInterval and legacy model objects should be migrated.
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(Int(seconds*STSavedTimer.centisecondsPerSecond), forKey: K.centisecondsKey)
+        aCoder.encode(isFavorite, forKey: K.isFavoriteKey)
+    }
+    
+    required convenience init(coder aDecoder: NSCoder) {
+        let centiseconds = aDecoder.decodeInteger(forKey: K.centisecondsKey)
+        let isFavorite = aDecoder.decodeBool(forKey: K.isFavoriteKey)
+        self.init(seconds: Double(centiseconds)/STSavedTimer.centisecondsPerSecond, isFavorite: isFavorite)
+    }
+}
