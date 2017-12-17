fb7857b88a0df159bae10c1378a940c3e2d9b082
Alan Kantz <alan.kantz@gmail.com>
2017-12-17 17:14:48 -0500

    Creating scaffolding for more unit tests

---
+++ TimerSwipeTests/TableDragDropTests.swift
@@ -0,0 +1,44 @@
+//
+//  TableDragDropTests.swift
+//  TimerSwipeTests
+//
+//  Created by Alan Kantz on 12/17/17.
+//  Copyright © 2017 Alan Kantz. All rights reserved.
+//
+
+import XCTest
+@testable import TimerSwipe
+
+class TableDragDropTests: XCTestCase {
+    
+    let tableController = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: StoryboardID.tableView.rawValue) as? TableController
+    let tableModelDDD: TableModelDragDropDelegate = {
+        class TableModelDDD: NSObject, TableModelDragDropDelegate {
+            func updateModelOnDrop(_ sourcePaths: [IndexPath], targetIndexPath: IndexPath) -> Bool {
+                return true
+            }
+        }
+        
+        return TableModelDDD.init()
+    }()
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testTDDDInitialization() {
+        guard let tableController = tableController else {
+            XCTFail()
+            return
+        }
+        let _ = TableDragDropDelegate.init(tableController, tableModelDragDropDelegate: tableModelDDD)
+        
+    }
+    
+}
