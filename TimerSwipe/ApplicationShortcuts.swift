349bd1d2bfeb818b70d3e17c96122c04d2f07d65
Alan Kantz <alan.kantz@gmail.com>
2018-01-26 15:34:58 -0500

    Added an object for handling the creation of quick application shortcuts

---
+++ TimerSwipe/ApplicationShortcuts.swift
@@ -0,0 +1,48 @@
+//
+//  ApplicationShortcuts.swift
+//  TimerSwipe
+//
+//  Created by Alan Kantz on 1/26/18.
+//  Copyright © 2018 Alan Kantz. All rights reserved.
+//
+
+import UIKit
+
+struct ApplicationShortcuts {
+    let systemDefinedMaxShortcuts = 4
+    let type = "timer"
+
+    var existingShortcuts: [UIApplicationShortcutItem]? {
+        get { return UIApplication.shared.shortcutItems }
+    }
+    
+    // It might be nice if the model and this object shared a serial queue to move a bunch of work off the main thread…
+    
+    func updateShortcuts(from timerList: STTimerList) {
+        let count = timerList.count()
+        
+        // If there are no timers, clear all the shortcut items
+        guard count > 0 else {
+            UIApplication.shared.shortcutItems = nil
+            return
+        }
+        
+        // Number of shortcuts can't exceed system max or number of timers
+        let logicalMaxShortcuts = count < systemDefinedMaxShortcuts ? count : systemDefinedMaxShortcuts
+        let userInfoKey = type
+
+        var newShortcuts = [UIApplicationShortcutItem]()
+        
+        // Create the shortcut and add it to the newShortcuts array
+        for index in 0..<logicalMaxShortcuts {
+            let userInfoValue = timerList[index]
+            let seconds = Int(userInfoValue.seconds)
+            let localizedTitle = NSLocalizedString("quickActionTitle", value: "\(seconds)-Second Timer", comment: "A timer of [seconds]-second duration")
+            let userInfo = [userInfoKey : userInfoValue]
+
+            let shortcut = UIApplicationShortcutItem.init(type: type, localizedTitle: localizedTitle, localizedSubtitle: nil, icon: nil, userInfo: userInfo)
+            newShortcuts.append(shortcut)
+        }
+        UIApplication.shared.shortcutItems = newShortcuts
+    }
+}
