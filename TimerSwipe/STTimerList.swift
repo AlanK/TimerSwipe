8578bf37806d06e7dd46dbe97c6b0fc4c171de28
Alan Kantz <alan.kantz@gmail.com>
2018-01-28 19:59:53 -0500

    Removed serial queue dispatch from updateShortcuts

--- TimerSwipe/STTimerList.swift
+++ TimerSwipe/STTimerList.swift
@@ -132,38 +132,35 @@ import UIKit
 
 extension STTimerList {
     private func updateShortcuts() {
-        serialQueue.async {
-            let systemDefinedMaxShortcuts = 4
-            let type = ShortcutTypes.timer.rawValue
-            let count = self.timers.count
-            
-            // If there are no timers, clear all the shortcut items
-            guard count > 0 else {
-                UIApplication.shared.shortcutItems = nil
-                return
-            }
-            
-            // Number of shortcuts can't exceed system max or number of timers
-            let logicalMaxShortcuts = count < systemDefinedMaxShortcuts ? count : systemDefinedMaxShortcuts
-            let userInfoKey = type
-            let icon = UIApplicationShortcutIcon.init(type: UIApplicationShortcutIconType.time)
-            
-            var newShortcuts = [UIApplicationShortcutItem]()
-            
-            // Create the shortcut and add it to the newShortcuts array
-            for index in 0..<logicalMaxShortcuts {
-                let timer = self.timers[index]
-                let seconds = Int(timer.seconds)
-                let localizedTitle = NSLocalizedString("quickActionTitle", value: "\(seconds)-Second Timer", comment: "A timer of [seconds]-second duration")
-                let userInfo = [userInfoKey : seconds]
-                
-                let shortcut = UIApplicationShortcutItem.init(type: type, localizedTitle: localizedTitle, localizedSubtitle: nil, icon: icon, userInfo: userInfo)
-                newShortcuts.append(shortcut)
-            }
-            DispatchQueue.main.async {
-                UIApplication.shared.shortcutItems = newShortcuts
-            }
+        let systemDefinedMaxShortcuts = 4
+        let type = ShortcutTypes.timer.rawValue
+        let count = timers.count
+        
+        // If there are no timers, clear all the shortcut items
+        guard count > 0 else {
+            UIApplication.shared.shortcutItems = nil
+            return
         }
+        
+        // Number of shortcuts can't exceed system max or number of timers
+        let logicalMaxShortcuts = count < systemDefinedMaxShortcuts ? count : systemDefinedMaxShortcuts
+        let userInfoKey = type
+        let icon = UIApplicationShortcutIcon.init(type: UIApplicationShortcutIconType.time)
+        
+        var newShortcuts = [UIApplicationShortcutItem]()
+        
+        // Create the shortcut and add it to the newShortcuts array
+        for index in 0..<logicalMaxShortcuts {
+            let timer = timers[index]
+            let seconds = Int(timer.seconds)
+            let localizedTitle = NSLocalizedString("quickActionTitle", value: "\(seconds)-Second Timer", comment: "A timer of [seconds]-second duration")
+            let userInfo = [userInfoKey : seconds]
+            
+            let shortcut = UIApplicationShortcutItem.init(type: type, localizedTitle: localizedTitle, localizedSubtitle: nil, icon: icon, userInfo: userInfo)
+            newShortcuts.append(shortcut)
+        }
+        
+        DispatchQueue.main.async { UIApplication.shared.shortcutItems = newShortcuts }
     }
 }
 
