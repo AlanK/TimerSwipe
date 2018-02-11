2d3fbd12c8d424d36860f7aa8ab0cf95eed73b7a
Alan Kantz <alan.kantz@gmail.com>
2018-02-11 13:15:07 -0500

    Created a time announcement preference object

---
+++ TimerSwipe/TimeAnnouncementPreference.swift
@@ -0,0 +1,13 @@
+//
+//  TimeAnnouncementPreference.swift
+//  TimerSwipe
+//
+//  Created by Alan Kantz on 2/11/18.
+//  Copyright © 2018 Alan Kantz. All rights reserved.
+//
+
+import UIKit
+
+struct TimeAnnounceMentPreference {
+    
+}
