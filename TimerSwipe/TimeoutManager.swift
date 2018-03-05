efb5fc6900c8d0541920efd76401e9f9c476dda2
Alan Kantz <alan.kantz@gmail.com>
2018-03-05 12:56:14 -0500

    Slight refactoring of TimeoutManager

--- TimerSwipe/TimeoutManager.swift
+++ TimerSwipe/TimeoutManager.swift
@@ -8,26 +8,37 @@
 
 import Foundation
 
-/// Implements a timeout behavior after the background has been in the background for a long time
+/// Implements a timeout behavior after the app has been in the background for a long time
 class TimeoutManager {
-    private let timeout: TimeInterval = 300.0
+    // MARK: Dependencies
+    
+    private let timeout: TimeInterval
+    private var appStateNotifications: AppStateNotifications
 
-    private var lastEnteredBackground: Date?
-    
-    private var appStateNotifications = AppStateNotifications()
-    
-    init(didTimeout: @escaping () -> Void) {
-        appStateNotifications.add(onBackground: { [unowned self] in
-            self.lastEnteredBackground = Date()
-            }, onActive: { [unowned self] in
-            if let date = self.lastEnteredBackground, self.timeout < Date().timeIntervalSince(date) {
-                didTimeout()
-            }
-            self.lastEnteredBackground = nil
-        })
+    // MARK: Initializers
+
+    init(_ timeout: TimeInterval = 300.0, appStateNotifications: AppStateNotifications = AppStateNotifications(), didTimeout: @escaping () -> Void) {
+        self.timeout = timeout
+        self.appStateNotifications = appStateNotifications
+        
+        monitorTimeout(didTimeout)
     }
     
-    deinit {
-        appStateNotifications.removeAll()
+    deinit { appStateNotifications.removeAll() }
+
+    // MARK: Properties
+    
+    private var lastEnteredBackground: Date?
+    
+    // MARK: Methods
+    
+    private func monitorTimeout(_ didTimeout: @escaping () -> Void) {
+        self.appStateNotifications.add(onBackground: { [unowned self] in self.lastEnteredBackground = Date() },
+                                       onActive: { [unowned self] in
+                                        if let date = self.lastEnteredBackground, self.timeout < Date().timeIntervalSince(date) {
+                                            didTimeout()
+                                        }
+                                        self.lastEnteredBackground = nil
+        })
     }
 }
