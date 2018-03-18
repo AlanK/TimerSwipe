c44ee6937e236c2ed21adeaa368d891cef6b33a7
Alan Kantz <alan.kantz@gmail.com>
2018-03-18 10:51:25 -0400

    TimeoutManager is now strictly an initializer with no methods or properties.

--- TimerSwipe/TimeoutManager.swift
+++ TimerSwipe/TimeoutManager.swift
@@ -10,25 +10,9 @@ import Foundation
 
 /// Implements a timeout behavior after the app has been in the background for a long time
 struct TimeoutManager {
-    // MARK: Dependencies
-    
-    private let timeout: TimeInterval
-    private let nc: NotificationCenter
-
     // MARK: Initializers
-
+    
     init(_ timeout: TimeInterval = 300.0, nc: NotificationCenter = NotificationCenter.default, didTimeout: @escaping () -> Void) {
-        self.timeout = timeout
-        self.nc = nc
-        
-        observeTimeout(didTimeout)
-    }
-    
-    // MARK: Methods
-    
-    private func observeTimeout(_ didTimeout: @escaping () -> Void) {
-        let timeout = self.timeout
-        let nc = self.nc
         
         // Create a background observation that will capture when the app enters the background and create an active observation…
         _ = nc.addObserver(forName: .UIApplicationDidEnterBackground, object: nil, queue: nil) { _ in
@@ -42,4 +26,5 @@ struct TimeoutManager {
             }
         }
     }
+    
 }
